111e1e5433545457ed85816305d05ab293eb3eae
Jimmey-Jiang <2181130@qq.com>
2018-01-17 09:26:16 +0800

    “service”

--- seckill/src/test/java/org/seckill/dao/SeckillDaoTest.java
+++ seckill/src/test/java/org/seckill/dao/SeckillDaoTest.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -45,14 +48,28 @@ public  class SeckillDaoTest {
 
     @Test
     public void reduceNumber() throws Exception {
+        long seckillId=1000;
+        Date date=new Date();
+        int updateCount=seckillDao.reduceNumber(seckillId,date);
+        System.out.println(updateCount);
     }
 
     @Test
     public void queryById() throws Exception {
+        long id=1000;
+        Seckill seckill = seckillDao.queryById(id);
+       // Seckill seckill=seckillDao.queryById(seckillId);
+       // System.out.println(seckill.getName());
+        System.out.println(seckill);
     }
 
     @Test
     public void queryAll() throws Exception {
+        List<Seckill> seckills=seckillDao.queryAll(0,100);
+        for (Seckill seckill : seckills)
+        {
+            System.out.println(seckill);
+        }
     }
 
 }
